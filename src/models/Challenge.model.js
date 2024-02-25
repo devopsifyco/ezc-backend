6d00942c0cf54672bab94100747f5d9dc28fc42e
ThaiHoang <thaihoang20112k3@gmail.com>
2024-02-25 16:40:59 +0700

    con/re modify challenge model

--- src/models/Challenge.model.js
+++ src/models/Challenge.model.js
@@ -11,7 +11,12 @@ const challengeSchema = new mongoose.Schema({
         required: true,
     },
     images_path: {
-        type: [String],
+        type: [
+            {
+                path: { type: String, required: true },
+                downloadLink: { type: String, required: true }
+            }
+        ],
         required: true,
     },
     description: {
